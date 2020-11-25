b9602a0951f03535d951b84e3ac193fea778037b
eleos <peaceaanu247@gmail.com>
2020-11-25 05:43:20 +0100

    my first commit

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title> ElletBlog | Home </title>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- stylesheet -->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+
+    <!-- Google font -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&family=Montserrat:wght@300;400&display=swap" rel="stylesheet"> 
+
+</head>
+<body>
+
+    <div class="page-wrapper">
+
+    <!-- Navigation bar starts -->
+                <div class="header">
+                    <div>
+                        <li><img src="imgs/elletblog_logo.png" alt="website logo" height="80" width="200"></li>
+                    </div>
+                    <div>
+                        <ul>
+                            <li><a href="index.php">HOME</a></li>
+                            <li><a href="#">ABOUT </a></li>
+                            <li><a href="#"> QUOTES </a></li>
+                            <li><a href="#"> CONTACT </a></li>
+                            <li><a href="#"><?php date_default_timezone_set("Africa/Lagos"); echo date("Y/m/d h:i:sa"); ?></a></li>
+                        </ul>
+                    </div>
+                </div>
+
+            <div class="slider">
+
+            </div>
+        
+          
+
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+    <script src="js/bootstrap.js"></script>
+</body>
+</html>
